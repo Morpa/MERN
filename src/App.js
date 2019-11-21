0558c8934172cf3a51eedae11d849b7cbbc740d9
André Morpanini <andremorpanini@MacBook-Pro-de-Andre.local>
2019-11-21 14:07:25 +0000

    Add React Router Dom and initial structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Users} />
+        <Route path="/places/new" exact component={NewPlace} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
