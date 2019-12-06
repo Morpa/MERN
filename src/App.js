10e923e4c69f2825f5a561e0665a122a0d22134f
André Morpanini <andremorpanini@MBP-de-Andre.Home>
2019-12-06 11:36:09 +0000

    Add form-hook

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" exact component={Users} />
           <Route path="/:userId/places" exact component={UserPlaces} />
           <Route path="/places/new" exact component={NewPlace} />
+          <Route path="/places/:placeId" exact component={UpdatePlace} />
           <Redirect to="/" />
         </Switch>
       </main>
