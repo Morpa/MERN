4156eefe51f55ad9e1cd4cb2a3b9bcc84acd5bea
André Morpanini <andremorpanini@MacBook-Pro-de-Andre.local>
2019-12-18 15:35:44 +0000

    Updating Places

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import Card from '../../shared/components/UIElements/Card';
-import PlaceItem from './PlaceItem';
-import Button from '../../shared/components/FormElements/Button';
-import './PlaceList.css';
+import Card from "../../shared/components/UIElements/Card";
+import PlaceItem from "./PlaceItem";
+import Button from "../../shared/components/FormElements/Button";
+import "./PlaceList.css";
 
 const PlaceList = props => {
   if (props.items.length === 0) {
@@ -23,7 +23,7 @@ const PlaceList = props => {
         <PlaceItem
           key={place.id}
           id={place.id}
-          image={place.imageUrl}
+          image={place.image}
           title={place.title}
           description={place.description}
           address={place.address}
